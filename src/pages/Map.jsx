1053467fdecb45dd7a4802d4c24af73cc107f5b4
Caleb Fernandes <calebfernandes3@gmail.com>
2023-09-17 10:37:19 -0400

    LATEST VERSION: CHANGE LOCATION

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,11 +1,10 @@
 // TODO: Restrict API key
 import firebase from "firebase/compat/app";
 import React from "react";
-import { useMemo } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import { useEffect, useState, useContext } from "react";
 import { GlobalVariables } from "../App";
-import { getDocs, getDoc, collection, addDoc } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
 import { db } from "../config/firebase-config";
 
 // CHANGE HERE
@@ -22,44 +21,60 @@ export default function Home() {
 
 function Map() {
   const [activeMarker, setActiveMarker] = useState(false);
-  const [activeMarkerFriend, setActiveMarkerFriend] = useState(false);
+  const [consentButton, setConsentButton] = useState(false);
   const { longitude, latitude } = useContext(GlobalVariables);
 
   const consent = async (ç) => {
     try {
+      setActiveMarker((prev) => !prev);
+      setConsentButton((prev) => !prev);
       await addDoc(locationsRef, {
         latitude: latitude,
         longitude: longitude,
         time: firebase.firestore.FieldValue.serverTimestamp(),
-        consent: true,
       });
     } catch (err) {
       console.log(err);
     }
   };
 
-  useEffect(() => {
-    setActiveMarker((prev) => true);
-  });
+  const FirstMap = () => {
+    return (
+      <GoogleMap zoom={3} center={{ lat: 0, lng: 0 }}>
+        <div className="h-screen">
+          <Marker position={{ lat: latitude, lng: longitude }} />
+        </div>
+        <button
+          className="fixed bottom-10 right-10 rounded-lg hover:bg-turq hover:text-flat bg-flat border-2 border-turq text-turq h-1/4"
+          onClick={consent}
+        >
+          Show Location
+        </button>
+      </GoogleMap>
+    );
+  };
 
-  return (
-    <GoogleMap zoom={10} center={{ lat: latitude, lng: longitude }}>
-      <div className="h-screen">
-        {activeMarker && (
-          <>
+  const NewMap = () => {
+    return (
+      <GoogleMap zoom={10} center={{ lat: latitude, lng: longitude }}>
+        <div className="h-screen">
+          <div>
             <Marker position={{ lat: latitude, lng: longitude }} />
-            {activeMarkerFriend && (
-              <Marker position={{ lat: latFriend, lng: lonFriend }} />
-            )}
-          </>
-        )}
-      </div>
-      <button
-        className="fixed bottom-10 right-10 rounded-lg hover:bg-turq hover:text-flat bg-flat border-2 border-turq text-turq h-1/4"
-        onClick={consent}
-      >
-        Consent 💖
-      </button>
-    </GoogleMap>
-  );
+          </div>
+        </div>
+        <button
+          className="fixed bottom-10 right-10 rounded-lg hover:bg-turq hover:text-flat bg-flat border-2 border-turq text-turq h-1/4"
+          onClick={consent}
+        >
+          Remove Location
+        </button>
+      </GoogleMap>
+    );
+  };
+
+  if (consentButton) {
+    return <NewMap />;
+  } else {
+    return <FirstMap />;
+  }
 }
